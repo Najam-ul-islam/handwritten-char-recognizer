3ee2228179f684c87b5ea7d1cd783250a2a17906
Muhammad Najam Ul Islam <26120402+Najam-ul-islam@users.noreply.github.com>
2021-11-30 18:41:34 +0500

    optimisitation with libs

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import numpy as np
 from keras.models import load_model
-import cv2
+# import cv2
 import base64
 from PIL import Image
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from io import BytesIO
 import json
 app = Flask(__name__)
@@ -27,15 +27,14 @@ def check_char():
     file = req['img']
     bytes_img = Image.open(BytesIO(base64.b64decode(file)))
     np_img = np.array(bytes_img)
-    img_gray = cv2.cvtColor(np_img, cv2.COLOR_BGR2GRAY)
-    _, img_thresh = cv2.threshold(img_gray, 100, 255, cv2.THRESH_BINARY_INV)
-    img_final = cv2.resize(img_thresh, (28, 28))
+    # img_gray = cv2.cvtColor(np_img, cv2.COLOR_BGR2GRAY)
+    # _, img_thresh = cv2.threshold(img_gray, 100, 255, cv2.THRESH_BINARY_INV)
+    # img_final = cv2.resize(np_img, (28, 28))
+    img_final = np.resize(np_img, (28, 28))
     img_final = np.reshape(img_final, (1, 28, 28, 1))
-    img = img_final // 255
+    # img = img_final / 255
     # ====================================
     # ====================================
-    fig, axes = plt.subplots(3, 3, figsize=(8, 9))
-    axes = axes.flatten()
     img_pred = model.predict(img_final)
     result = {"Predictions": img_pred,
               "img": img_final.shape,
